451c846121d17306833f92e59907ac2eac2c5ce7
ibrahimyilmaz00 <halilyilmaz3343@gmail.com>
2026-03-16 15:20:20 +0300

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -50,6 +51,7 @@ export default function RootLayout({
             >
                 {children}
             </body>
+            <GoogleAnalytics gaId="G-HYK55E1KJH" />
         </html>
     );
 }
